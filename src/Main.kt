477aa90e8f23e60923fdd6f9385c73e5df8f6c8b
erick <erickcardenamaque@gmail.com>
2026-09-12 00:57:46 -0500

    mensaje

--- src/Main.kt
+++ src/Main.kt
@@ -2,5 +2,5 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
-    println("Hello, soy un asistente universitario, mucho gusto")
+    println("Hola, soy erick y estoy aprendiendo kotlin")
 }
